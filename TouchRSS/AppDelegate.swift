78497222cad185fbed5e6ea3bf595e8b94094592
Atsushi Yasui @ MBA2 <a.yasui@gmail.com>
2016-12-20 14:08:03 +0900

    Initial Commit

---
+++ TouchRSS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchRSS
+//
+//  Created by At Yasui on 2016/12/20.
+//  Copyright © 2016年 At Yasui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchRSS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchRSS
+//
+//  Created by At Yasui on 2016/12/20.
+//  Copyright © 2016年 At Yasui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
