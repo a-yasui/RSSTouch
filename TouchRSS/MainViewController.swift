ad90b30112702b8d59c7b2e6fe17b15b9fc44a81
Atsushi Yasui @ MBA2 <a.yasui@gmail.com>
2016-12-21 19:40:16 +0900

    削除

--- TouchRSS/MainViewController.swift
+++ TouchRSS/MainViewController.swift
@@ -15,43 +15,6 @@ class MainViewController: NSViewController
     override func viewDidAppear() {
         
         NSLog("View Did Appears")
-//        let attributeText = NSMutableAttributedString(string: "テスト")
-//        
-//        let field = TRTextField(frame: NSRect(x: 0, y: 0, width: 30, height: 30))
-//        let attr = [
-////            NSLinkAttributeName: "https://google.com/",
-//        :] as [String : Any]
-//        
-//        attributeText.beginEditing()
-//        let range = NSRange(location: 0, length: attributeText.length)
-//        attributeText.addAttributes(attr, range: range)
-//        attributeText.endEditing()
-//        
-//        let test = NSMutableAttributedString(string:"|")
-//        
-//        test.beginEditing()
-//        test.append(attributeText)
-//        test.append(NSAttributedString(string: "|"))
-//        let attr1 = [
-//            NSForegroundColorAttributeName: NSColor.orange,
-//            NSStrikethroughColorAttributeName: NSColor.orange,
-//            NSStrokeColorAttributeName: NSColor.orange,
-//            NSUnderlineStyleAttributeName: NSUnderlineStyle.styleNone.rawValue,
-//            NSFontAttributeName: NSFont(name: "HelveticaNeue-Bold", size: 21.0)!,
-//        ] as [String : Any]
-//        test.addAttributes(attr1, range: NSRange(location:0, length: test.length))
-//        test.endEditing()
-//        
-//        field.attributedStringValue = test
-//        field.drawsBackground = false
-//        field.isBordered = false
-//        field.isEditable = false
-//        field.isSelectable = true
-//        field.action = #selector(MainViewController.click(_:))
-//        field.sizeToFit()
-//        
-//        self.view.addSubview(field)
-        
     }
     
     func click(_ test: AnyObject)
@@ -59,11 +22,4 @@ class MainViewController: NSViewController
         NSLog("hogehoge \(test)")
     }
     
-}
-
-extension MainViewController: NSTextFieldDelegate
-{
-    func textField(_ textField: NSTextField, textView: NSTextView, shouldSelectCandidateAt index: Int) -> Bool {
-        return true
-    }
 }
